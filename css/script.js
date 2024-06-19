81786957f716ca381c539b01dd1cda063704de87
Hung-tech1309 <hungnhgts00736@fpt.edu.vn>
2024-06-19 23:09:30 +0700

    init project

---
+++ css/script.js
@@ -0,0 +1,11 @@
+function applySettings() {
+    
+    var backgroundColor = document.getElementById('background-color').value;
+    var fontFamily = document.getElementById('font-family').value;
+
+    
+    document.body.style.backgroundColor = backgroundColor;
+
+   
+    document.body.style.fontFamily = fontFamily;
+}
